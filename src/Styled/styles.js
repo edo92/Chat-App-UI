9cc42e761f6b402f2795135edea69d1d93a7c342
Eduard Jacobs <edoter92@gmail.com>
2021-05-15 19:00:30 -0700

    cleanup styled/styles

--- src/Styled/styles.js
+++ src/Styled/styles.js
@@ -5,15 +5,6 @@ import {
   activePrimary,
 } from 'Styled/colors';
 
-export const button = css`
-  display: flex;
-  justify-content: center;
-  border-radius: 7px;
-  transition: all 0.23s;
-  position: relative;
-  cursor: pointer;
-`;
-
 export const HighlightOnHover = css`
   &: hover {
     background-color: ${highlightActive};
